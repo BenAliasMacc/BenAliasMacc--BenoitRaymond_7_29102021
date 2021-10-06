c99379edb1556cbf7fb61cb710f7e81b082c0ee6
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2021-10-06 16:34:12 +0200

    recherche principal et secondaires ok

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 import { recipes } from "./recipes.js";
 console.log(recipes);
 // DOM
-const formPrincipal = document.querySelector(".recherche-principale");
+const input = document.querySelectorAll("input");
+const recherchePrincipal = document.querySelector(".recherche-principale");
 const inputPrincipal = document.querySelector(".recherche-principale__input");
-const form = document.querySelector("recherche-principal");
+const rechercheParIngredients = document.querySelector(
+  ".recherche-par-ingredients"
+);
+const inputIngredients = document.querySelector(
+  ".recherche-par-ingredients__input"
+);
+const rechercheParAppareil = document.querySelector(".recherche-par-appareil");
+const inputAppareil = document.querySelector(".recherche-par-appareil__input");
+const rechercheParUstensiles = document.querySelector(
+  ".recherche-par-ustensiles"
+);
+const inputUstensiles = document.querySelector(
+  ".recherche-par-ustensiles__input"
+);
+const listeIngredients = document.querySelector(".liste-ingredients");
 const cartes = document.querySelector(".cartes");
 
 // Variables globales
 let contenuInput = "";
 let arrayRecipes = [];
+let arrayIngredients = [];
+let arrayAppareil = [];
+let arrayUstensiles = [];
 let arrayTrie = [];
+let arrayTrieIngredients = [];
+let arrayTrieAppareil = [];
+let arrayTrieUstensiles = [];
 
 // Affichage
-const affichagePlats = () => {
-  arrayRecipes.forEach((recipe) => {
+const affichagePlats = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let recipe = array[i];
     let listeIngredients = "";
     recipe.ingredients.forEach((ingredient) => {
       if (!ingredient.hasOwnProperty("quantity")) {
@@ -57,31 +79,49 @@ const affichagePlats = () => {
           </div>
         </article>
        `;
-  });
+  }
+};
+
+const affichagelisteIngredients = () => {
+  for (let i = 0; i < arrayIngredients.length; i++) {
+    let recipe = arrayIngredients[i];
+    recipe.ingredients.forEach((ingredient) => {
+      listeIngredients.innerHTML += `<li>${ingredient.ingredient}</li>`;
+    });
+  }
 };
 
 // Trie
+// Recherche principale
 function trie() {
   for (let i = 0; i < recipes.length; i++) {
     arrayTrie[
       i
     ] = `${recipes[i].name} - ${recipes[i].description} - ${recipes[i].appliance}`;
+    arrayTrieAppareil[i] = `${recipes[i].appliance} `;
 
     for (let j = 0; j < recipes[i].ingredients.length; j++) {
       if (
         recipes[i].ingredients[j].ingredient &&
         recipes[i].ustensils[j] != undefined
       ) {
-        arrayTrie[i] += ` - ${recipes[i].ingredients[j].ingredient}`;
-        arrayTrie[i] += ` - ${recipes[i].ustensils[j]}`;
+        arrayTrie[
+          i
+        ] += ` - ${recipes[i].ingredients[j].ingredient} - ${recipes[i].ustensils[j]}`;
+      } else {
+        arrayTrie[i] += ` - ${recipes[i].ingredients[j].ingredient} `;
       }
+      arrayTrieIngredients[i] += `${recipes[i].ingredients[j].ingredient} `;
+      arrayTrieUstensiles[i] += `${recipes[i].ustensils[j]}`;
     }
   }
 }
 
 trie();
 
+// Recherche principal
 function recherche(value) {
+  // console.log(arrayTrie);
   for (let i = 0; i < arrayTrie.length; i++) {
     if (arrayTrie[i].toLowerCase().includes(value.toLowerCase())) {
       arrayRecipes.push(recipes[i]);
@@ -89,21 +129,105 @@ function recherche(value) {
   }
 }
 
-// Barre de recherche
 inputPrincipal.addEventListener("input", (e) => {
   contenuInput = e.target.value.toLowerCase();
 });
 
-formPrincipal.addEventListener("input", (e) => {
+recherchePrincipal.addEventListener("input", (e) => {
   if (inputPrincipal.value.length >= 3) {
     e.preventDefault();
     cartes.innerHTML = "";
     arrayRecipes = [];
     recherche(contenuInput);
-    affichagePlats();
+    affichagePlats(arrayRecipes);
   } else {
     e.preventDefault();
     cartes.innerHTML = "";
     arrayRecipes = [];
+  }
+});
+
+// Recherches secondaire
+// Ingrédients
+function rechercheIngredients(value) {
+  for (let i = 0; i < arrayTrieIngredients.length; i++) {
+    if (arrayTrieIngredients[i].toLowerCase().includes(value.toLowerCase())) {
+      arrayIngredients.push(recipes[i]);
+    }
+  }
+}
+
+inputIngredients.addEventListener("input", (e) => {
+  contenuInput = e.target.value.toLowerCase();
+});
+
+rechercheParIngredients.addEventListener("input", (e) => {
+  if (inputIngredients.value.length >= 3) {
+    e.preventDefault();
+    cartes.innerHTML = "";
+    arrayIngredients = [];
+    rechercheIngredients(contenuInput);
+    affichagePlats(arrayIngredients);
+  } else {
+    e.preventDefault();
+    cartes.innerHTML = "";
+    arrayIngredients = [];
+  }
+});
+
+// Appareil
+function rechercheAppareil(value) {
+  console.log(value);
+  for (let i = 0; i < arrayTrieIngredients.length; i++) {
+    if (arrayTrieAppareil[i].toLowerCase().includes(value.toLowerCase())) {
+      arrayAppareil.push(recipes[i]);
+    }
+  }
+}
+
+inputAppareil.addEventListener("input", (e) => {
+  contenuInput = e.target.value.toLowerCase();
+});
+
+rechercheParAppareil.addEventListener("input", (e) => {
+  if (inputAppareil.value.length >= 3) {
+    e.preventDefault();
+    console.log("test");
+    cartes.innerHTML = "";
+    arrayAppareil = [];
+    rechercheAppareil(contenuInput);
+    affichagePlats(arrayAppareil);
+  } else {
+    e.preventDefault();
+    cartes.innerHTML = "";
+    arrayAppareil = [];
+  }
+});
+
+//Ustensiles
+function rechercheUstensiles(value) {
+  console.log(value);
+  for (let i = 0; i < arrayTrieIngredients.length; i++) {
+    if (arrayTrieUstensiles[i].toLowerCase().includes(value.toLowerCase())) {
+      arrayUstensiles.push(recipes[i]);
+    }
+  }
+}
+
+inputUstensiles.addEventListener("input", (e) => {
+  contenuInput = e.target.value.toLowerCase();
+});
+
+rechercheParUstensiles.addEventListener("input", (e) => {
+  if (inputUstensiles.value.length >= 3) {
+    e.preventDefault();
+    cartes.innerHTML = "";
+    arrayUstensiles = [];
+    rechercheUstensiles(contenuInput);
+    affichagePlats(arrayUstensiles);
+  } else {
+    e.preventDefault();
+    cartes.innerHTML = "";
+    arrayUstensiles = [];
   }
 });
